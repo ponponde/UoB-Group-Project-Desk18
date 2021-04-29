f426381abe80ea5dfc3ff9958c813b96e3eb4b41
David Lan <31298786+benjamin5252@users.noreply.github.com>
2021-04-29 16:04:49 +0100

    fix test

--- mern/ui/src/components/MusicPlayer/__test__/MusicPlayer.test.js
+++ mern/ui/src/components/MusicPlayer/__test__/MusicPlayer.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MusicPlayer from './../index';
+import MusicPlayer from '../index';
 import { render, cleanup } from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
 import renderer from "react-test-renderer";
