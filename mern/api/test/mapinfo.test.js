007a9327221960831c610d49021a602a2c2578ba
Iris Ho <learncodecool@gmail.com>
2021-05-10 15:12:41 +0100

    fix test

--- mern/api/test/mapinfo.test.js
+++ mern/api/test/mapinfo.test.js
@@ -40,24 +40,23 @@ describe("GET /api/map/mapInfo/:id", function () {
         supertest(app)
             .get("/api/map/mapInfo/GB")
             .then((res) => {
-              const {body} = res;
-              expect(body).to.have.property("Country");
-              expect(body).to.have.property("CountryCode");
-              expect(body).to.have.property("Confirmed");
-              expect(body).to.have.property("Deaths");
-              expect(body).to.have.property("Recovered");
-              expect(body).to.have.property("Active");
-              expect(body).to.have.property("Recommendation");
-              expect(body).to.have.property("Notes");
-              expect(body).to.have.property("NewConfirmed");
-              expect(body).to.have.property("statistics");
-              expect(body).to.have.property("rankList");
+                const { body } = res;
+                expect(body).to.have.property("Country");
+                expect(body).to.have.property("CountryCode");
+                expect(body).to.have.property("Confirmed");
+                expect(body).to.have.property("Deaths");
+                expect(body).to.have.property("Recovered");
+                expect(body).to.have.property("Active");
+                expect(body).to.have.property("Recommendation");
+                expect(body).to.have.property("Notes");
+                expect(body).to.have.property("statistics");
+                expect(body).to.have.property("rankList");
             })
-          .then(() => {
-            done();
-          })
-          .catch((error) => {
-            done(error);
-          });
-  });
+            .then(() => {
+                done();
+            })
+            .catch((error) => {
+                done(error);
+            });
+    });
 });
