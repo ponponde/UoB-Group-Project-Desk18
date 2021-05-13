bb008ec7818dcbb514b9fdaf9f5f22454326aae8
David Lan <31298786+benjamin5252@users.noreply.github.com>
2021-05-13 17:02:44 +0100

    Update index.js
    solve first editor test conflict

--- mern/ui/src/components/Editor/index.js
+++ mern/ui/src/components/Editor/index.js
@@ -27,7 +27,6 @@ const Editor = () => {
     return (
         
             <div
-                data-testid="Editor"
                 style={{
                     textAlign: "right",
                 }}
